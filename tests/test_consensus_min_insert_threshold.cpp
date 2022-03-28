d445a9fba1cbce489adf453d330a1c9a4ede9cea
cmaceves <christineaceves22@gmail.com>
2022-03-28 16:09:20 -0700

    adding part of test for longer insertions

--- tests/test_consensus_min_insert_threshold.cpp
+++ tests/test_consensus_min_insert_threshold.cpp
@@ -3,7 +3,7 @@
 #include "../src/allele_functions.h"
 
 int main() {
-  int num_tests = 8;
+  int num_tests = 10;
   allele a1 = {
     "T",
     8,
@@ -59,6 +59,52 @@ int main() {
   std::cout << s.nuc << ": " << s.q << std::endl;
   success += (s.nuc.compare("T") == 0) ? 1: 0;
   success += (s.q.compare("?") == 0) ? 1 : 0;
+    
+  //adding a test to check snv against insertion threshold
+  allele a3 = {
+    "T",
+    5,
+    0,
+    30,
+    0,
+    0,
+    0
+  };
+  allele a4 = {
+    "+GGGGGGG",
+    3,
+    0,
+    30,
+    0,
+    0,
+    0
+  };
+  allele a5 = {
+    "A",
+    2,
+    0,
+    30,
+    0,
+    0,
+    0
+  };
 
+  allele arr1[] = {a3, a4, a5};
+  std::vector<allele> ad1(arr1, arr1+sizeof(arr1)/sizeof(allele));
+  
+ for(int i = 0;i<size;i++){
+    ad1.at(i) = arr1[i];
+  }
+
+  //not bothering with single insertion, should be checked elsewhere
+  //first param is 1bp second is >1bp 
+  s = get_consensus_allele(ad1,20,0, 'N',0.8, 0);
+  std::cout << s.nuc << ": " << s.q << std::endl;
+  success += (s.nuc.compare("TGGGGGGG") == 0) ? 1: 0;
+   
+  s = get_consensus_allele(ad1,20, 0, 'N',0, 0.8);
+  std::cout << s.nuc << ": " << s.q << std::endl;
+  success += (s.nuc.compare("T") == 0) ? 1: 0;
+  
   return (success == num_tests) ? 0 : -1;
 }
